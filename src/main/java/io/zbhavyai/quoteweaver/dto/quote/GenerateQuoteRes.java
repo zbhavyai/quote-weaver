1cab14aa5bb4d4035aff54d26e80fe3f5ff6f3bf
Bhavyai Gupta <zbhavyai@gmail.com>
2025-08-25 11:47:49 -0600

    add description annotation in the inference response

--- src/main/java/io/zbhavyai/quoteweaver/dto/quote/GenerateQuoteRes.java
+++ src/main/java/io/zbhavyai/quoteweaver/dto/quote/GenerateQuoteRes.java
@@ -1,7 +1,8 @@
 package io.zbhavyai.quoteweaver.dto.quote;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.langchain4j.model.output.structured.Description;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
-public record GenerateQuoteRes(@JsonProperty("quote") String quote) {}
+public record GenerateQuoteRes(@JsonProperty("quote") @Description("quote") String quote) {}
