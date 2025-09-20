31057285d20c2cc00d49608adf5d340f7db6853a
Bhavyai Gupta <zbhavyai@gmail.com>
2025-09-20 13:59:46 -0600

    change pre-commit hook wording from lint to format

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -9,7 +9,7 @@ block() {
     exit 1
 }
 
-lint_checks() {
+format_checks() {
     local staged
     staged=$(git diff --name-only --cached --exit-code -- '*.java')
     ret=$?
@@ -17,7 +17,7 @@ lint_checks() {
         return 0
     fi
 
-    (./mvnw --quiet spotless:check) || block "[ERROR] Lint checks failed; run 'make format' to fix."
+    (./mvnw --quiet --batch-mode spotless:check) || block "[ERROR] Formatting checks failed; run 'make format' to fix."
 }
 
-(lint_checks) || exit $?
+(format_checks) || exit $?
