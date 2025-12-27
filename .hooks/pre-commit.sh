f5992fd813456f184d9f6f2beb1c7a14abbfaa16
Bhavyai Gupta <zbhavyai@gmail.com>
2025-12-27 12:06:55 -0700

    update pre-commit hook

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-block() {
+function block() {
     echo -e "\n\n"
     echo "$@"
     echo "[ERROR] Commit blocked."
     exit 1
 }
 
-format_checks() {
-    local staged
-    staged=$(git diff --name-only --cached --exit-code -- '*.java')
-    ret=$?
-    if [ $ret -eq 0 ]; then
+function format_checks() {
+    mapfile -d '' -t staged_java < <(git diff --cached --name-only -z --diff-filter=ACMR -- '*.java' || true)
+
+    if ((${#staged_java[@]} == 0)); then
         return 0
     fi
 
-    (./mvnw --quiet --batch-mode spotless:check) || block "[ERROR] Formatting checks failed; run 'make format' to fix."
+    if ! ./mvnw --quiet --batch-mode spotless:check; then
+        block "[ERROR] Formatting checks failed; run 'make format' to fix."
+    fi
 }
 
 (format_checks) || exit $?
