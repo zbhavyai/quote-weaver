8fad377f37b10e1c3f3e5c7b7849d1b0df8d331e
Bhavyai Gupta <zbhavyai@gmail.com>
2025-12-27 17:23:45 -0700

    update lifecycle scope for this class

--- src/main/java/io/zbhavyai/quoteweaver/init/AppLifecycle.java
+++ src/main/java/io/zbhavyai/quoteweaver/init/AppLifecycle.java
@@ -2,11 +2,11 @@ package io.zbhavyai.quoteweaver.init;
 
 import io.quarkus.runtime.Shutdown;
 import io.quarkus.runtime.Startup;
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationScoped
+@Singleton
 public class AppLifecycle {
 
   private static final Logger LOG = LoggerFactory.getLogger(AppLifecycle.class);
